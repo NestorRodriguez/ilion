c0db8da0db0fd028308ff8de8fa5e19f4c516bbd
Maikol Ramirez <maikol.ramirez@chevyplan.com.co>
2019-12-04 08:21:32 -0500

    Servicios

---
+++ Backend .net/WsRestConsultaWebAsamblea.LN/Consultas/consulta_estudiante.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WsRestConsultaWebAsamblea.LN.Consultas
+{
+    public class consulta_estudiante
+    {
+    }
+}
