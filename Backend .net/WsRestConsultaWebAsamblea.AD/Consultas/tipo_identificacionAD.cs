b801ebc669389cbdbfc798134abe1219ebff2445
maikop12345 <maikol.123@hotmail.com>
2019-12-07 07:21:12 -0500

    Servicios

--- Backend .net/WsRestConsultaWebAsamblea.AD/Consultas/tipo_identificacionAD.cs
+++ Backend .net/WsRestConsultaWebAsamblea.AD/Consultas/tipo_identificacionAD.cs
@@ -57,5 +57,105 @@ namespace WsRestConsultaWebAsamblea.AD.Consultas
             return listrest;
 
         }
+
+        public bool posttipo_identificacion(tipo_identificacion obj)
+        {
+
+            bool confirmacion = false;
+            string[,,] Param = new string[1, 3, 1];
+
+
+            Param[0, 0, 0] = obj.nom_ident;
+            Param[0, 1, 0] = "@nom_ident";
+            Param[0, 2, 0] = "varchar(40)";
+            
+            try
+            {
+                string resul = wsc.Ejecutar(Param, "OPE_post_tipo_identificacion", "ilion");
+
+                if (resul.Equals("1"))
+                {
+                    confirmacion = true;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message + "Error ");
+                confirmacion = false;
+            }
+
+            return confirmacion;
+
+
+        }
+
+        public bool puttipo_identificacion(tipo_identificacion obj)
+        {
+
+            bool confirmacion = false;
+            string[,,] Param = new string[2, 3, 1];
+            Param[0, 0, 0] = obj.id_ident;
+            Param[0, 1, 0] = "@id_ident";
+            Param[0, 2, 0] = "nvarchar(40)";
+
+            Param[1, 0, 0] = obj.nom_ident;
+            Param[1, 1, 0] = "@nom_ident";
+            Param[1, 2, 0] = "nvarchar(40)";
+
+            try
+            {
+                string resul = wsc.Ejecutar(Param, "OPE_put_tipo_identificacion", "ilion");
+
+                if (resul.Equals("1"))
+                {
+                    confirmacion = true;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message + "Error ");
+                confirmacion = false;
+            }
+
+            return confirmacion;
+
+
+        }
+
+        public bool deletetipo_identificacion(tipo_identificacion obj)
+        {
+
+            bool confirmacion = false;
+            string[,,] Param = new string[1, 3, 1];
+            Param[0, 0, 0] = obj.id_ident;
+            Param[0, 1, 0] = "@id_ident";
+            Param[0, 2, 0] = "nvarchar(40)";
+
+
+
+            try
+            {
+                string resul = wsc.Ejecutar(Param, "OPE_delete_tipo_identificacion", "ilion");
+
+                if (resul.Equals("1"))
+                {
+                    confirmacion = true;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message + "Error ");
+                confirmacion = false;
+            }
+
+            return confirmacion;
+
+
+        }
+
+
     }
 }
--- Backend .net/WsRestConsultaWebAsamblea.LN/Consultas/Consulta_Identificacion.cs
+++ Backend .net/WsRestConsultaWebAsamblea.LN/Consultas/Consulta_Identificacion.cs
@@ -11,11 +11,11 @@ namespace WsRestConsultaWebAsamblea.LN.Consultas
     public class Consulta_Identificacion
     {
 
+        List<tipo_identificacion> LisRest = new List<tipo_identificacion>();
+        tipo_identificacion ObjError = new tipo_identificacion();
+
         public List<tipo_identificacion> Consulta_ListIdentificacion()
         {
-
-            List<tipo_identificacion> LisRest = new List<tipo_identificacion>();
-            tipo_identificacion ObjError = new tipo_identificacion();
 
             try
             {
@@ -47,14 +47,8 @@ namespace WsRestConsultaWebAsamblea.LN.Consultas
         public List<tipo_identificacion> Consulta_Identificacion_id(tipo_identificacion obj)
         {
 
-            List<tipo_identificacion> LisRets = new List<tipo_identificacion>();
-            tipo_identificacion ObjError = new tipo_identificacion();
-
-            try
+                      try
             {
-
-                if (obj.id_ident.Equals(""))
-                {
 
                     tipo_identificacion Objrest = new tipo_identificacion();
                     Objrest.id_ident = obj.id_ident;
@@ -71,14 +65,7 @@ namespace WsRestConsultaWebAsamblea.LN.Consultas
                         LisRets.Add(ObjError);
                         return LisRets;
                     }
-                }
-                else
-                {
-                    ObjError.Error = "El numero de id no existe";
-                    LisRets.Add(ObjError);
-                    return LisRets;
-                }
-
+               
             }
             catch (Exception)
             {
@@ -87,5 +74,97 @@ namespace WsRestConsultaWebAsamblea.LN.Consultas
                 return LisRets;
             }
         }
+
+        public tipo_identificacion posttipo_identificacion(tipo_identificacion obj)
+        {
+            try
+            {
+
+                tipo_identificacion Objhap = new tipo_identificacion();
+                Objhap.nom_ident = obj.nom_ident;
+
+                tipo_identificacionAD ObjhadAD = new tipo_identificacionAD();
+                bool resultado = ObjhadAD.posttipo_identificacion(Objhap);
+                if (resultado == true)
+                {
+                    ObjError.Error = "Se agrego Exitosamente";
+                    return ObjError;
+                }
+                else
+                {
+                    ObjError.Error = "No se agrego ";
+                    return ObjError;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ObjError.Error = "Error" + ex.Message;
+                return ObjError;
+
+            }
+        }
+
+        public tipo_identificacion puttipo_identificacion(tipo_identificacion obj)
+        {
+            try
+            {
+
+                tipo_identificacion Objhap = new tipo_identificacion();
+                Objhap.id_ident = obj.id_ident;
+                Objhap.nom_ident = obj.nom_ident;
+                
+                tipo_identificacionAD ObjhadAD = new tipo_identificacionAD();
+                bool resultado = ObjhadAD.puttipo_identificacion(Objhap);
+                if (resultado == true)
+                {
+                    ObjError.Error = "Se actualizo Exitosamente";
+                    return ObjError;
+                }
+                else
+                {
+                    ObjError.Error = "No se actualizo ";
+                    return ObjError;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ObjError.Error = "Error" + ex.Message;
+                return ObjError;
+
+            }
+        }
+
+        public tipo_identificacion deletetipo_identificacion(tipo_identificacion obj)
+        {
+            try
+            {
+
+                tipo_identificacion Objhap = new tipo_identificacion();
+                Objhap.id_ident = obj.id_ident;
+
+                tipo_identificacionAD ObjhadAD = new tipo_identificacionAD();
+                bool resultado = ObjhadAD.deletetipo_identificacion(Objhap);
+                if (resultado == true)
+                {
+                    ObjError.Error = "Se elimino Exitosamente";
+                    return ObjError;
+                }
+                else
+                {
+                    ObjError.Error = "No se elimino ";
+                    return ObjError;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ObjError.Error = "Error" + ex.Message;
+                return ObjError;
+
+            }
+        }
+
     }
 }
