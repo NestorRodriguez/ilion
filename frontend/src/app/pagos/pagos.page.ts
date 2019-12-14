cd731de19645b6cdfda6a569b124ec9854cefbd5
maikop12345 <maikol.123@hotmail.com>
2019-12-14 04:50:51 -0500

    Cambios

--- frontend/src/app/pagos/pagos.page.ts
+++ frontend/src/app/pagos/pagos.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagos.page.scss'],
 })
 export class PagosPage implements OnInit {
-
+  enlace = '';
   constructor() { }
 
   ngOnInit() {
   }
 
+  // tslint:disable-next-line: only-arrow-functions
+  abrirNequi() {
+    window.open('https://recarga.nequi.com.co/bdigitalpsl/#!/', '_system', 'location=yes');
+  }
+
+  abrirDaviPlata() {
+    window.open('https://www.psepagos.co/PSEHostingUI/ShowTicketOffice.aspx?ID=2508', '_system', 'location=yes');
+  }
 }
