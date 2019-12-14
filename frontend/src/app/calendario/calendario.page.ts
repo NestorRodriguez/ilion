a414337902479064e60f5502fc1610d918bcd7ff
maikop12345 <maikol.123@hotmail.com>
2019-12-14 00:51:41 -0500

    Cambios

--- frontend/src/app/calendario/calendario.page.ts
+++ frontend/src/app/calendario/calendario.page.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/Core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgCalendarModule  } from 'ionic2-calendar';
 
-@Component({
-  selector: 'app-calendario',
-  templateUrl: './calendario.page.html',
-  styleUrls: ['./calendario.page.scss'],
-})
+
 export class CalendarioPage implements OnInit {
 
-  constructor() { }
+  constructor(private commonModule: CommonModule,
+              private ionicModule: IonicModule,
+              private formsModule: FormsModule,
+              private routerModule: RouterModule,
+              private ngCalendarModule: NgCalendarModule
+    ) { }
 
   ngOnInit() {
   }
