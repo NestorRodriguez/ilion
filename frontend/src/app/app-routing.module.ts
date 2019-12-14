e7058649d33ef63c43d71524e75a3b6bd4fac02b
maikop12345 <maikol.123@hotmail.com>
2019-12-14 05:03:41 -0500

    Cambios

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'logo', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'registro', loadChildren: './registro/registro.module#RegistroPageModule' },
   { path: 'msjexitoso', loadChildren: './msjexitoso/msjexitoso.module#MsjexitosoPageModule' },
@@ -19,7 +19,8 @@ const routes: Routes = [
   { path: 'resumeninscrip', loadChildren: './resumeninscrip/resumeninscrip.module#ResumeninscripPageModule' },
   { path: 'menu-estadisticas', loadChildren: './menu-estadisticas/menu-estadisticas.module#MenuEstadisticasPageModule' },
   { path: 'nutricion', loadChildren: './nutricion/nutricion.module#NutricionPageModule' },
-  { path: 'posicion', loadChildren: './posicion/posicion.module#PosicionPageModule' },  { path: 'calendar', loadChildren: './calendar/calendar.module#CalendarPageModule' },
+  { path: 'posicion', loadChildren: './posicion/posicion.module#PosicionPageModule' },
+  { path: 'calendar', loadChildren: './calendar/calendar.module#CalendarPageModule' },
 
 
 
